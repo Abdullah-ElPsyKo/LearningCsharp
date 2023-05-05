a426d51393a033acf1abaee947e5f28c4eca2632
Abdulla <abdullahbagishev@icloud.com>
2023-05-06 01:59:27 +0200

    week 7 exercise added: little mini-game

---
+++ Exercises/week7/Ganzenbord Dams Van Camp editie/Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ganzenbord_Dams_Van_Camp_editie
+{
+    public class Dobbelsteen
+    {
+        static public int Rol()
+        {
+            Random rangen = new Random();
+            int getal = rangen.Next(1, 4);
+            return getal;
+        }
+    }
+    public class Speelvakje
+    {
+        public int BeweegVakjes { get; private set; }
+        public Speelvakje()
+        {
+            Random rangen = new Random();
+            int kansen = rangen.Next(1, 101);
+            if (kansen < 51)
+            {
+                BeweegVakjes = 0;
+            }
+            else if (kansen > 50 && kansen < 81)
+            {
+                kansen = rangen.Next(1, 101);
+                if (kansen < 51)
+                {
+                    BeweegVakjes = 1;
+                }
+                else
+                {
+                    BeweegVakjes = 2;
+                }
+            }
+            else
+            {
+                kansen = rangen.Next(1, 101);
+                if (kansen < 51)
+                {
+                    BeweegVakjes = -1;
+                }
+                else
+                {
+                    BeweegVakjes = -2;
+                }
+            }
+        }
+        virtual public void ToonVakje()
+        {
+            if (BeweegVakjes >= 0)
+            {
+                Console.Write("+" + BeweegVakjes);
+            }
+            else
+            {
+                Console.Write(BeweegVakjes);
+            }
+        }
+
+    }
+    public class Ganzenbord
+    {
+        private List<Speelvakje> SpeelVakjes = new List<Speelvakje>();
+        public Ganzenbord()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SpeelVakjes.Add(new Speelvakje());
+            }
+        }
+        public int pionIndex;
+        public int HuidigeScore { get; set; } = 0;
+
+        public bool BeweegPion(int a)
+        {
+            pionIndex += a;
+            if (pionIndex > 9)
+            {
+                HuidigeScore += 10;
+                return true;
+            }
+            else
+            { 
+                pionIndex += SpeelVakjes[pionIndex].BeweegVakjes;
+                Console.WriteLine(pionIndex);
+
+                if (pionIndex <= 8 && pionIndex >= 0)
+                {
+                    return false;
+                }
+                else if (pionIndex > 8)
+                {
+                    return true;
+                }
+                else
+                {
+                    HuidigeScore -= 10;
+                    pionIndex = 0; //reset
+                    return false;
+                }
+            }
+        }
+        public void TekenBord()
+        {
+            for (int i = 0; i < SpeelVakjes.Count; i++)
+            {
+                if (pionIndex == i)
+                {
+                    if (SpeelVakjes[i].BeweegVakjes < 0)
+                    {
+                        Console.Write("-T");
+                    }
+                    else
+                    {
+                        Console.Write("+T");
+                    }
+                    
+                }
+                else
+                {
+                    SpeelVakjes[i].ToonVakje();
+                }
+            }
+        }
+    }
+}
---
+++ Exercises/week7/Ganzenbord Dams Van Camp editie/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ganzenbord_Dams_Van_Camp_editie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ganzenbord bord = new Ganzenbord();
+            while (true)
+            {
+                Console.Clear();
+                bord.TekenBord();
+                int dbrol = Dobbelsteen.Rol();
+                Console.WriteLine($"\nJe rolde {dbrol}");
+                Console.ReadLine();
+                if (bord.BeweegPion(dbrol))
+                {
+                    Console.WriteLine("Proficiat, je hebt gewonnen.");
+                    break;
+                }
+            }
+            Console.WriteLine("Je score is: " + bord.HuidigeScore); // display the final score
+            Console.ReadLine();
+        }
+    }
+}

80ca7711f3e2ff40fdfad5c01af09719492abbc1
Abdulla <abdullahbagishev@icloud.com>
2023-05-18 20:30:54 +0200

    week 8 exercise added: interfaces

---
+++ Exercises/week8/Carbon Footprint/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbon_Footprint
+{
+    internal class Class1
+    {
+        public interface ICarbonFootPrint
+        {
+            double BerekenFootprint();
+            void VerlaagFootprint();
+        }
+        public class Huis : ICarbonFootPrint
+        {
+            public int volume { get; set; } //carbonfootprint based on volume
+            private int footprintMp = 10;
+            public double BerekenFootprint()
+            {
+                return volume * footprintMp;
+            }
+            public void VerlaagFootprint()
+            {
+                footprintMp -= 1;
+            }
+        }
+        public class Fabriek : ICarbonFootPrint
+        {
+            public int aantalWerknemers { get; set; } //carbonfootprint based on amount of workers
+            private int workersEfficiency = 100; //default is 100
+            public double BerekenFootprint()
+            {
+                return aantalWerknemers * workersEfficiency;
+            }
+            public void VerlaagFootprint()
+            {
+                workersEfficiency -= 1;
+            }
+        }
+        public class Auto : ICarbonFootPrint
+        {
+            public string merkAuto { get; set; } //carbonfootprint based on what car
+            string[] autoMerken = {"bmw", "mercedes", "audi", "volkswagen"};
+            double[] carbonFootprints = { 2.5, 3.0, 2.8, 2.2 };
+
+            public double BerekenFootprint()
+            {
+                for (int i = 0; i < autoMerken.Length; i++)
+                {
+                    if (merkAuto == autoMerken[i])
+                    {
+                        return carbonFootprints[i];
+                    }
+                }
+                return 0.0;
+            }
+            public void VerlaagFootprint()
+            {
+                for (int i = 0; i < autoMerken.Length; i++)
+                {
+                    if (merkAuto == autoMerken[i])
+                    {
+                        carbonFootprints[i] -= 0.2; //car got more efficient somehow
+                    }
+                }
+            }
+        }
+        public class Plant
+        {
+        }
+    }
+}
---
+++ Exercises/week8/Carbon Footprint/Program.cs
@@ -0,0 +1,52 @@
+﻿using static Carbon_Footprint.Class1;
+
+namespace Carbon_Footprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> vervuilers = new List<object>();
+            vervuilers.Add(new Plant());
+            vervuilers.Add(new Huis() { volume = 200 });
+            vervuilers.Add(new Fabriek() { aantalWerknemers = 10 });
+            vervuilers.Add(new Auto() { merkAuto = "audi" });
+            vervuilers.Add(new Plant());
+            vervuilers.Add(new Huis() { volume = 100 });
+            vervuilers.Add(new Fabriek() { aantalWerknemers = 25 });
+            vervuilers.Add(new Auto() { merkAuto = "bmw" });
+
+            double carbonfootprint = 0;
+            double grootsteVervuiling = 0;
+            int grootsteVervuilerIndex = 0;
+
+            for (int i = 0; i < vervuilers.Count; i++)
+            {
+
+                var vervuilverI = vervuilers[i] as ICarbonFootPrint;
+                if (vervuilverI != null)
+                {
+                    vervuilverI.VerlaagFootprint(); //carbon footprint lowered on all objects
+                    double vervuiling = vervuilverI.BerekenFootprint();
+                    Console.WriteLine($"{vervuilers[i].GetType()}:{vervuiling}");
+                    carbonfootprint += vervuiling;
+                    if (vervuiling > grootsteVervuiling)
+                    {
+                        grootsteVervuiling = vervuiling;
+                        grootsteVervuilerIndex = i;
+                    }
+                }
+                else
+                {
+                    carbonfootprint -= 0.005; //let's say one plant processes 5kg of carbon a year
+
+                }
+            }
+            Console.WriteLine($"\nGemiddelde footprint van alle objecten: {carbonfootprint / vervuilers.Count}.\nGrootste footprint: {grootsteVervuiling} door {vervuilers[grootsteVervuilerIndex].GetType()}");
+            if (grootsteVervuilerIndex >= 0)
+            {
+
+            }
+        }
+    }
+}
